eb2e06b9bea492a53fcbb65692c20ef78c7e0309
SantiagoBejarano <bejaranosh@gmail.com>
2022-03-29 22:13:39 -0300

    Post Clase 29/03

--- ProjectoPrueba/AppCoder/urls.py
+++ ProjectoPrueba/AppCoder/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import curso, entregables, estudiantes, profesores, inicio
 
 urlpatterns = [
-    path('', inicio),
-    path('agrega.curso/<nombre>/<camada>/', curso),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables),
+    path('', inicio, name='Inicio'),
+    path('agrega.curso/<nombre>/<camada>/', curso, name='Cursos'),
+    path('profesores/', profesores, name='Profesores'),
+    path('estudiantes/', estudiantes, name='Estudiantes'),
+    path('entregables/', entregables, name='Entregables'),
 ]
--- ProjectoPrueba/AppCoder/views.py
+++ ProjectoPrueba/AppCoder/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Curso
 
+
 # Create your views here.
 def curso(request, nombre, camada):
     mi_curso = Curso(nombre=nombre, camada=camada)
     mi_curso.save()
 
     return render(request, 'AppCoder/cursos.html', {'nombre':nombre,'camada':camada})
-
-    return HttpResponse(f'Se genero el curso de {mi_curso.nombre} con la camada {mi_curso.camada}')
-
+    
 def profesores(request):
     return render(request, 'AppCoder/profesores.html')
 
@@ -21,4 +20,4 @@ def entregables(request):
     return render(request, 'AppCoder/entregables.html')
 
 def inicio(request):
-    return HttpResponse(f'Vista de inicio')
+    return render(request, 'AppCoder/inicio.html')
